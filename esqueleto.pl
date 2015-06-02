63674987adb6df4c03ae6343771b0e2cc4aaedaf
santiaboy2 <santiaboy2@gmail.com>
2015-06-02 17:35:04 -0300

    un ejemplo y agrego all

--- esqueleto.pl
+++ esqueleto.pl
@@ -29,7 +29,7 @@ tablero(dona3x3, T) :- tablero(3, 3, T), ocupar(pos(1, 1), T).
 tablero(cueva5x5, T) :- tablero(5, 5, T), ocupar(pos(1, 1), T), ocupar(pos(1, 2), T), ocupar(pos(1, 3), T), ocupar(pos(2, 3), T), ocupar(pos(3, 1), T), ocupar(pos(3, 2), T), ocupar(pos(3, 3), T).
 tablero(zigzag5x5restringido, T) :- tablero(5, 6, T), ocupar(pos(1, 1), T), ocupar(pos(0, 2), T), ocupar(pos(3, 1), T), ocupar(pos(3, 2), T), ocupar(pos(2, 3), T), ocupar(pos(1, 4), T), ocupar(pos(3, 5), T), ocupar(pos(4, 5), T), ocupar(pos(3, 4), T).
 tablero(zigzag5x5, T) :- tablero(5, 6, T), ocupar(pos(1, 1), T), ocupar(pos(0, 2), T), ocupar(pos(3, 1), T), ocupar(pos(3, 2), T), ocupar(pos(2, 3), T), ocupar(pos(1, 4), T).
-
+tablero(chorizo6x3, T) :- tablero(6, 3, T), ocupar(pos(1, 1), T), ocupar(pos(2, 1), T),ocupar(pos(3, 1), T),ocupar(pos(4, 1), T). %tablero(chorizo6x3, T)
 
 %%No chekea si la posicion es valida, se deberia hacer previamente.
 casilleroLibre(pos(F,C),T) :- nth0(F,T,F1), nth0(C,F1,X), var(X).
@@ -125,7 +125,7 @@ armarCamino2(S,F,T,C,X) :- vecinoEnOrden(S,F,V), movValido(V,T,X),armarCamino2(V
 camino3(S,F,T,C) :- posValida(S,T), posValida(F,T), assertz(llegueEn(F,0)), armarCamino3(F,S,T,C,[]).
 
 armarCamino3(pos(SX,SY),pos(SX,SY),_,[pos(SX,SY) | X],X).
-armarCamino3(S,F,T,C,X) :- vecinoEnOrden(S,F,V), movValido(V,T,X), length(X,L1), L is L1+1, not(tardoMasQueAntes(V,L)), retract(llegueEn(V,_)), assertz(llegueEn(V,L)), armarCamino3(V,F,T,C,[S|X]).
+armarCamino3(S,F,T,C,X) :- vecinoEnOrden(S,F,V), movValido(V,T,X), length(X,L1), L is L1+1, not(tardoMasQueAntes(V,L)), retractall(llegueEn(V,_)), assertz(llegueEn(V,L)), armarCamino3(V,F,T,C,[S|X]).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%
